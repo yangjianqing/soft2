798d2ec50c9da63d78d77768d770c135cf17c259
chuan <877533957@qq.com>
2024-05-13 18:23:26 +0800

    2024/5/13  tgc:绿源鲜选超市管理系统标题及图片修改提交

--- lanqiao-ui/src/views/login.vue
+++ lanqiao-ui/src/views/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login">
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form">
-      <h3 class="title">蓝桥管理系统</h3>
+      <h3 class="title">绿源鲜选超市管理系统</h3>
       <el-form-item prop="username">
         <el-input
           v-model="loginForm.username"
