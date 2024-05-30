be51033a0edafc0e213c7a9c7a9c94ba037efafb
chuan <877533957@qq.com>
2024-05-30 18:52:18 +0800

    2024/5/30 Chuan:新增收银页面。

---
+++ lanqiao-admin/src/main/java/cn/lanqiao/web/controller/system/collectmoneyController.java
@@ -0,0 +1,16 @@
+package cn.lanqiao.web.controller.system;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 收银页面Controller
+ *
+ * @author lanqiao
+ * @date 2024-05-30
+ */
+@RestController
+@RequestMapping("/system/collectmoney")
+public class collectmoneyController {
+
+}
---
+++ lanqiao-ui/src/views/system/collectmoney/index.vue
@@ -0,0 +1,183 @@
+<template>
+  <div class="warp">
+    <el-row :gutter="5" align="middle">
+      <el-col :span="18">
+        <h1>收银台</h1>
+        <div class="warp_info">
+          <el-row :gutter="5" align="middle">
+            <el-col :span="4">
+              <p style="height: 40px;line-height: 40px">
+                商品条码：
+              </p>
+            </el-col>
+            <el-col :span="18">
+              <p style="height: 40px;line-height: 40px">
+                <el-input
+                  v-model="barcode"
+                  placeholder="请扫描商品条形码"
+                  @input="handleBarcodeInput"
+                  @keyup.enter.native="checkout"
+                ></el-input>
+              </p>
+            </el-col>
+          </el-row>
+        </div>
+        <div class="warp_info wrap_table" >
+          <el-table :data="productsInCart" style="width: 100%"     :row-class-name="tableRowClassName">
+            <el-table-column prop="name" label="商品名称"></el-table-column>
+            <el-table-column prop="price" label="价格"></el-table-column>
+            <el-table-column prop="quantity" label="数量">
+              <template slot-scope="scope">
+                <el-button
+                  size="mini"
+                  icon="el-icon-minus"
+                  type="primary"
+                  circle
+                  @click="handleDecrease(scope.$index)"
+                  :disabled="scope.row.quantity === 1"
+                ></el-button>
+                <span>&nbsp;&nbsp;{{ scope.row.quantity }}&nbsp;&nbsp;</span>
+                <el-button
+                  size="mini"
+                  type="primary"
+                  icon="el-icon-plus"
+                  circle
+                  @click="handleIncrease(scope.$index)"
+                ></el-button>
+              </template>
+            </el-table-column>
+            <el-table-column label="操作">
+              <template slot-scope="scope">
+                <el-button icon="el-icon-delete" type="danger" circle @click="removeFromCart(scope.$index)"></el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </div >
+        <div class="warp_info warp_bottom">
+          <p>总价：{{ calculateTotalPrice }}</p>
+          <el-button type="success" size="medium" @click="checkout">结账</el-button>
+        </div>
+      </el-col>
+      <el-col :span="6">
+        <div class="pay_img">
+          <img src="@/assets/images/collectmoney/wechat.png" alt="微信支付" />
+          <img src="@/assets/images/collectmoney/alipay.png" alt="支付宝支付" />
+          <img src="@/assets/images/collectmoney/cash.png" alt="现金支付" />
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      barcode: '', //条码
+      products: [
+        { id: 1, name: '商品A', price: 10.0},
+        { id: 2, name: '商品B', price: 20.0 },
+        // ... 其他商品
+      ],
+      productsInCart: [],
+    };
+  },
+  methods: {
+    removeFromCart(index){
+      this.productsInCart.splice(index,1);
+    },
+
+
+    handleIncrease(index) {
+      this.productsInCart[index].quantity++;
+      this.calculateTotalPrice();
+    },
+    handleDecrease(index) {
+      if (this.productsInCart[index].quantity > 1) {
+        this.productsInCart[index].quantity--;
+        this.calculateTotalPrice();
+      }
+    },
+
+    handleBarcodeInput() {
+      //判断商品是否存在
+      const foundProduct = this.products.find(product => product.id === parseInt(this.barcode, 10));
+
+      if (foundProduct) {
+        const existingProduct = this.productsInCart.find(item => item.id === foundProduct.id);
+        if (existingProduct) {
+          existingProduct.quantity++;
+        } else {
+          this.productsInCart.push({ ...foundProduct, quantity: 1 });
+        }
+        this.barcode = ''; // 清空条形码输入
+      } else {
+        this.$message.error('未找到该商品');
+      }
+    },
+    checkout() {
+      // 实现结账逻辑，比如调用后端API进行支付
+      this.$message.success('结账成功');
+      this.productsInCart = [];
+    },
+
+  },
+  computed:{
+    calculateTotalPrice(){
+      return  this.productsInCart.reduce((sum, item) => sum + item.price * item.quantity, 0);
+    }
+  },
+
+};
+</script>
+
+<style scoped>
+h1 {
+  font-family: '黑体', '微软雅黑', sans-serif; /* 使用黑体或微软雅黑字体，如果找不到则使用无衬线字体 */
+  font-size: 48px; /* 设置字体大小 */
+  color: #0099ff; /* 设置字体颜色为红色 */
+  text-align: center; /* 文本居中显示 */
+  text-shadow: 2px 2px 4px rgba(173, 115, 53, 0.5); /* 设置文本阴影 */
+  margin-bottom: 20px; /* 设置标题下方的外边距 */
+}
+
+/* 样式可以根据需要进行自定义 */
+.el-table .warning-row {
+  background: oldlace;
+}
+
+.el-table .success-row {
+  background: #f0f9eb;
+}
+
+.warp{
+  width: 80%;
+  height: 600px;
+  margin: 30px auto;
+
+}
+
+.warp_info{
+  margin: 10px 15px;
+  background: #f1efef;
+  border-radius: 15px;
+  padding: 0 15px;
+}
+
+.wrap_table{
+  padding: 15px 15px;
+
+}
+
+.warp_bottom{
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.pay_img {
+  margin-top: 12rem;
+  width: 64px;
+  height: 64px;
+}
+</style>
